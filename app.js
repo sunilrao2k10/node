a04e81130082580517082095fd903e98b74ee2e1
sunilyadav <sunil.yadav@kelltontech.com>
2020-04-26 22:41:40 +0530

    mongoo connection changes

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(compression());
 mongoose.connect(process.env.MONGO_URI,{    
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology:true
+    useUnifiedTopology:false,    
 })
 .then(()=>{
   console.log('MongoDB connected');
